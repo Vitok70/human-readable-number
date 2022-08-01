45ef283c39513f7262c619a4c5b0d067cb27fcec
Vitok70 <vitokunev70@gmail.com>
2022-08-01 22:22:38 +0300

    feat:solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
-module.exports = function toReadable (number) {
-  
+module.exports = 
+function toReadable (n) {
+    let str = ''; strA =''; strB =''; strC ='';
+
+let zero = 'zero';
+let units = ['','one','two','three','four','five','six','seven','eight','nine','ten','eleven','twelve','thirteen','fourteen','fifteen','sixteen','seventeen','eighteen','nineteen','twenty'];
+let dozens = ['','','twenty','thirty','forty','fifty','sixty','seventy','eighty','ninety'];
+    
+if (n == 0){ return zero;}
+if (n <= 20) { return units[n];} 
+   else if (n <= 99) {
+        strB = dozens[Math.trunc(n/10)];
+        strA = units[n % 10];
+        str = strB +' '+ strA;
+        } 
+        else if (n <= 999) {
+             strC = units[Math.trunc(n/100)];
+               if (Math.trunc(n/10) % 10 == 0 ){
+               strA = units[n % 10];
+               str = strC +' '+ 'hundred'+' '+ strA;
+              } else if (Math.trunc(n/10) % 10 == 1){
+                      strB = units[n % 100];
+                      str = strC +' '+ 'hundred'+' '+ strB;
+                     }
+                     else { strB = dozens[Math.trunc(n/10) % 10];
+                          strA = units[(n % 100) % 10];
+                          str = strC +' '+ 'hundred' + ' '+ strB +' '+ strA;
+                          }
+            }
+    return str.trim();
 }
+// console.log(toReadable(919));
+
